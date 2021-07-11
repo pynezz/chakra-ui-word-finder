da053b11745a86900e4b327bb6858c35fe2f18b2
Kevin <kevin.ka_96@hotmail.com>
2021-07-11 21:15:03 +0200

    updated packages, it works, sort of...

---
+++ components/Words.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Words = ({data}) => {
+    return (
+        <div>
+            <p>{data}</p>
+        </div>
+    );
+}
+
+
+export default Words;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,6 @@
-import { Flex, Heading, Stack, CheckboxGroup, useColorMode, Button, Spacer, Input, useForceUpdate, FormHelperText } from '@chakra-ui/react'
-import Checkboxes from '../components/Checkboxes'
-import ResultData from '../components/ResultData'
-import { Children, useState } from 'react'
-// import ApiData from '../components/Api'
-// import ResComp from '../components/Api'
-
-
+import { Flex, Heading, Stack, CheckboxGroup, useColorMode, Button, Spacer, Input } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import Words from '../components/Words'
 
 const IndexPage = () => {
 
@@ -16,67 +11,22 @@ const IndexPage = () => {
   const API_URL = "https://api.datamuse.com/words?";
 
   // HOOKS
-  const [checkedItems, setCheckedItems] = useState([])
   const [inputValue, setInputValue] = useState('');
   const [response, setResponse] = useState([])
 
-  let jsonObj ={
+  useEffect(() => {
+    getWords('ml', 'boat');
+  }, []);
 
-      'ml':     [''],
-      'rel_syn':[''],
-      'rel_ant':[''],
-      'rel_spc':[''],
-      'rel_gen':[''],
-      'rel_rhy':['']
-  }
-  const dataMap = new Map<string, string[]>()
-  const [data, setData] = useState([''])
-
-  
-  let calls = ['']
-
-  const handleChange = ({ item, value }) => {
-    setCheckedItems(item);
-    calls.push(value)
-  }
-  let responses = ['', '']
   // THE API FUNCTION
-  const runApi = (search) =>  {
+  const getWords = async (param, search) => {
+    const response = await fetch(`${API_URL}${param}=${search}`);
+    const data = await response.json();
+    setResponse(data.word);
 
-    const r: number = 12
-    const maxResponses: string = `&max=${r}`
-    //search = 'another'
-    checkedItems.forEach( (element) => {
-      if (element) {                    // If an element is present
-        console.log(element)
-        let req = new XMLHttpRequest(); // Make a new request:
-        console.log(`search term: ${search}`)
-        req.open(
-          "GET",
-          `${API_URL}${element}=${search}${maxResponses}`
-        );
-        req.setRequestHeader("Accept", "application/json");
-        req.send();       // Send the request to the API with the checkbox's value as a parameter
-        req.onload = ()  =>  {
-          // When the request is done
-          if (req.status == 200) {
-            let res = req.response; // Assign the response to the string variable 'res'
-            //res = JSON.parse(res)
-            console.log('Response', res)
-            responses.push(element, res)
-            setResponse(res);
-            jsonObj[element] = JSON.parse(res);
-            dataMap.set(element, JSON.parse(res))
-            console.log(jsonObj)
-            setData(jsonObj[element]) //! DETTE FUNKA
-            //setData(JSON.parse(res))
-          }
-        };
-      }
-    })
-    console.log('data: ', data)//!FUNKER
-    //var data_ = jsonObj.map(v => v.data)
   }
+    //var data_ = jsonObj.map(v => v.data)
+  
 
   return (
     <Flex height="100vh" alignItems="center" justifyContent="flex-start" direction="column">
@@ -100,75 +50,11 @@ const IndexPage = () => {
             value={inputValue}
             onChange={(event) => setInputValue(event.target.value)}
           />
-          <Stack spacing={6}>
-            <CheckboxGroup 
-              colorScheme="blue" 
-              /*defaultValue={['ml']}*/ 
-              onChange={setCheckedItems} //setCheckedItems
-            >
-              <Flex direction="row"alignItems="left" >
-                <Checkboxes />
-              </Flex>
-              <Flex direction="row">
-            {/* { 
-              checkedItems.map(element => {
-                if (element) {
-                  return(
-                    <ResComp parameter={element} search={inputValue} maxRes={'&max=12'} category={element} />
 
-                    )
-                  }
-                })
-            
-              } */}
-              </Flex>
-            </CheckboxGroup>
+            <Words data={response}></Words>
 
-          </Stack>
           {/* <Button my={10} onClick={() => runApi(inputValue)}>Search</Button> */}
           {/* <ApiData parameter='ml' maxRes='&max=12' search={inputValue} /> */}
-
-
-
-                      {/* //! THE ISSUE IS HERE. IT DOES NOT GET THE LENGTH. IT IS NOT EMPTY, DOES IT RENDER BEFORE IT'S DONE?? */}
-              <ResultData heading = {'Similar'} results={jsonObj.ml.values.length} key={Math.floor(Math.random() * 1000)}/>
-              {/* <ResultData heading = {'Hyponyms'} results = {jsonObj['rel_spc']} key={Math.floor(Math.random() * 1000)}/>
-              <ResultData heading = {'Rhymes'} results = {data} key={Math.floor(Math.random() * 1000)}/> */}
-
-
-            {/* <ResultData 
-              heading={'Similar'}
-              results={data}
-            />
-
-            <ResultData 
-              heading={'Hypernyms'}
-              results={dataMap.size} 
-            />
-
-            <ResultData 
-              heading={'Hyponyms'}
-              results={dataMap.get('rel_gen')} 
-            />
-
-            <ResultData 
-              heading={'Synonyms'}
-              results={dataMap.get('rel_syn')} 
-            />
-
-            <ResultData 
-              heading={'Antonyms'}
-              results={dataMap.values} 
-            />
-
-            <ResultData 
-              heading={'Rhymes'}
-              results={dataMap.forEach(k => k.values.toString())} 
-            /> */}
-          
-
-          
-
         </Flex>
     </Flex>  
   )
